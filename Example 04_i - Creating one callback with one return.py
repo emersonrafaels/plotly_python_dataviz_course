2847d916023c0833877005d5fb9768d3069c4e1e
Emerson Rafael <emersona7x@hotmail.com>
2023-02-08 17:04:43 -0300

    [ADD] ADICIONANDO PRIMEIROS EXEMPLOS USANDO CALLBACK

---
+++ Example 04_i - Creating one callback with one return.py
@@ -0,0 +1,42 @@
+from dash import Dash
+from dash.html import Div, P, Br
+from dash.dcc import Input as DCCInput
+from dash.dependencies import Input, Output
+
+# INICIANDO O APP
+app = Dash(__name__)
+
+# DEFININDO OS WIDGETS QUE ESTARÃO EM TELA
+app.layout = Div(
+    children=[
+        DCCInput(id='meu_input1', value='batatinha frita'),
+        Br(),
+        P(id='output1'),
+    ]
+)
+
+# DEFININDO O CALLBACK
+"""
+
+    DCC1: AO ALTERAR O OBJECT COM ID: MEU_INPUT1 (QUE É UM INPUT)
+    
+    O CHILDREN DO OBJECT COM ID: OUTPUT1 (QUE É UMA TAG HTML P)
+    SOFRERÁ ALTERAÇÃO
+    
+    O CHILDREN RECEBERÁ O RETORNO DA FUNÇÃO DE CALLBACK.
+    
+"""
+
+@app.callback(
+    [
+        Output('output1', 'children'),
+    ],
+    [
+        Input('meu_input1', 'value')
+    ]
+)
+def meu_callback(meu_input1):
+    return meu_input1
+
+
+app.run_server()
