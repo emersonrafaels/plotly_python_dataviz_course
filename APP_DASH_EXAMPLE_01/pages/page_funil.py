3ce8508e6ee240320c2805454b08e46f77ad08ee
Emerson Rafael <emersona7x@hotmail.com>
2023-07-06 00:02:18 -0300

    [ADD] ADICIONANDO BOTÃO DE DOWNLOAD E GRÁFICO DE BARRAS

--- APP_DASH_EXAMPLE_01/pages/page_funil.py
+++ APP_DASH_EXAMPLE_01/pages/page_funil.py
@@ -1,80 +1,128 @@
+import random
+
+import pandas as pd
+import numpy as np
+
 import dash
 import dash.dcc as dcc
 import dash.html as html
 import dash_bootstrap_components as dbc
 import dash_ag_grid as dag
+import plotly.express as px
 
 from dynaconf import settings
 
 from app import *
-from utils.pandas_functions import load_data
+from utils.pandas_functions import load_data, dataframe_convert_dtypes_to_aggrid
 
 # DIRETÓRIO DE DADOS DO DATASET DE AGÊNCIAS
 dir_data = settings.DATA_DIR_AGENCIAS
 
+def create_data_funnel():
+
+    N_SAMPLES = 500
+
+    list_funnel = ["FUNNEL {}".format(str(np.random.randint(0, 7))) for _ in range(N_SAMPLES)]
+    list_index_location = random.sample(range(10000), N_SAMPLES)
+
+    dict_type_funnel = {"FUNNEL 1": "Potencial Encerramento",
+                        "FUNNEL 2": "Ags Reformadas (PD + EI)",
+                        "FUNNEL 3": "Remanejamento (Imóvel antigo)",
+                        "FUNNEL 4": "AVCB Crítico",
+                        "FUNNEL 5": "Itaú Rent",
+                        "FUNNEL 6": "Ags que serão reformadas (PD + EI)",
+                        "PD": "Saldo PD"}
+
+    list_funnel_updated = ["PD" if value == "FUNNEL 0" else value for value in list_funnel]
+
+    df = pd.DataFrame({"LOCATION": list_index_location, "GROUP FUNNEL": list_funnel_updated})
+
+    df['GROUP FUNNEL LABEL'] = df['GROUP FUNNEL'].replace(dict_type_funnel)
+
+    df_groupby_count_funnel = df.groupby(["GROUP FUNNEL", "GROUP FUNNEL LABEL"]).size().reset_index(name="QUANTIDADE")
+
+    return df, df_groupby_count_funnel
+
 # CARREGANDO OS DADOS
 df = load_data(dir_data)
+df_funnel, df_groupby_count_funnel = create_data_funnel()
+
+# BARPLOT
+fig_barplot = px.bar(df_groupby_count_funnel,
+                     x="GROUP FUNNEL",
+                     y="QUANTIDADE",
+                     color="GROUP FUNNEL LABEL",
+                     text_auto=True,
+                     labels={"GROUP FUNNEL LABEL": "GRUPO FUNIL"})
+
+# FORMATANDO O DATATABLE COM AGGRID
+grid = dag.AgGrid(
+    id="data_grid_funil",
+    columnDefs=[
+        {"field": column, "filter": field_type}
+        for column, field_type in dataframe_convert_dtypes_to_aggrid(df).items()
+    ],
+    rowData=df.to_dict("records"),
+    dashGridOptions={
+        "resizable": True,
+        "pagination": True,
+        "sortable": True,
+        "filter": True,
+        "animateRows": True,
+    },
+    className="ag-theme-alpine",
+    defaultColDef={"sortable": True},
+    csvExportParams={
+        "fileName": "BASE_FOOTPRINT_RESULTADO_FUNIL.csv",
+    },
+)
 
 # LAYOUT
 layout = html.Div(
     children=[
-        html.H1(children=""),
-        # DATATABLE
-        dash.dash_table.DataTable(
-            id="typing_formatting",
-            data=df.to_dict("records"),
-            # FORMATANDO AS COLUNAS
-            columns=[
-                {"id": "CÓDIGO AG", "name": "CÓDIGO DA AGÊNCIA", "type": "numeric"},
-                {"id": "UF", "name": "UF", "type": "text"},
+        html.Div(
+            children=[
+                # TÍTULO
+                html.H1(children="Resultado - Modelo do Funil"),
+                # DATATABLE
+                grid,
+                # LINHA SEPARADORA
+                html.Hr(),
+                # BOTÃO DE DOWNLOAD
+                html.Button(
+                    "Download Excel",
+                    id="excel-button",
+                    n_clicks=0,
+                    className="btn btn-outline-primary",
+                ),
+                dcc.Download(id="download-dataframe-xlsx"),
             ],
-            # OPÇÃO PARA SELEÇÃO DE LINHAS
-            row_selectable="single",
-            # OPÇÃO PARA DELETAR LINHAS
-            row_deletable=False,
-            # OPÇÃO PARA EDITAR VALORES
-            editable=False,
-            # OPÇÃO PARA FILTRAR
-            filter_action="native",
-            # OPÇÃO PARA ORDENA
-            sort_action="native",
-            # FORMATAÇÃO DE ESTILO DAS CÉLULAS
-            style_cell={"textAlign": "center"},
-            # FORMATAÇÃO DE ESTILO DA TABELA
-            style_table={"overflowX": "auto"},
-            style_data_conditional=[
-                {
-                    "if": {"column_editable": False},
-                    "backgroundColor": "rgb(30, 30, 30)",
-                    "color": "white",
-                }
+            style={"margin-top": 20, "margin-right": 20},
+        ),
+        html.Div(
+            children=[
+                # TÍTULO
+                html.H1(children="Resultado - Quantidade de ag. por etapa do funil"),
+                # GRÁFICO DE BARRAS
+                dcc.Graph(figure=fig_barplot),
             ],
-            style_header_conditional=[
-                {
-                    "if": {"column_editable": False},
-                    "backgroundColor": "rgb(30, 30, 30)",
-                    "color": "white",
-                }
-            ],
-            css=[
-                {
-                    "selector": ".dash-spreadsheet td div",
-                    "rule": """
-                                                line-height: 15px;
-                                                max-height: 30px; min-height: 30px;
-                                                display: block;
-                                                overflow-y: hidden;
-                                            """,
-                }
-            ],
-            tooltip_data=[
-                {
-                    column: {"value": str(value), "type": "markdown"}
-                    for column, value in row.items()
-                }
-                for row in df.to_dict("records")
-            ],
-            tooltip_duration=None,
-        )
+            style={"margin-top": 20, "margin-right": 20},
+        ),
     ]
 )
+
+# CRIANDO CALLBACK PARA DOWNLOAD DOS DADOS DA TABELA (data_grid_funil)
+@app.callback(
+    dash.Output("download-dataframe-xlsx", "data"),
+    dash.Input("excel-button", "n_clicks"),
+    prevent_initial_call=True,
+)
+def export_data_as_csv(n_clicks):
+    if n_clicks:
+        return dcc.send_data_frame(
+            df.to_excel,
+            "BASE_FOOTPRINT_RESULTADO_FUNIL.xlsx",
+            sheet_name="RESULTADO_FOOTPRINT",
+            index=None,
+        )
+    return False
--- APP_DASH_EXAMPLE_01/utils/pandas_functions.py
+++ APP_DASH_EXAMPLE_01/utils/pandas_functions.py
@@ -33,3 +33,35 @@ def filter_columns_multiselect(colunas_desejadas, df_columns):
         filter(lambda x: x in df_columns, colunas_desejadas_antes_depois)
     )
     return colunas_desejadas_filter
+
+
+def dataframe_get_types(dataframe):
+
+    return dataframe.dtypes
+
+
+def dataframe_convert_dtypes_to_aggrid(dataframe):
+
+    # INICIANDO A VARIÁVEL QUE ARMAZENARÁ O RESULTADO
+    dict_result = {}
+
+    aggrid_types = {
+        "object": "agTextColumnFilter",
+        "int64": "agNumberColumnFilter",
+        "float64": "agNumberColumnFilter",
+        "datetime64[ns]": "agDateColumnFilter",
+    }
+
+    valor_default = "agTextColumnFilter"
+
+    # VERIFICANDO SE O ARGUMENTO É UM DATAFRAME
+    if isinstance(dataframe, pd.DataFrame):
+
+        # OBTENDO O DTYPES DESSE DATAFRAME
+        dataframe_dtypes = dataframe_get_types(dataframe)
+
+        for key, value in dict(dataframe_dtypes).items():
+
+            dict_result[key] = aggrid_types.get(str(value), valor_default)
+
+    return dict_result
